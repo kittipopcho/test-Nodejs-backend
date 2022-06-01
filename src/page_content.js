d87ac1358458065a2fe00cde7653e5b8b37e0639
kittipop <kittipco@ais.co.th>
2022-06-01 23:14:10 +0700

    done all features

--- src/page_content.js
+++ src/page_content.js
@@ -11,15 +11,22 @@ export const getContentByText = async (text) => {
   let type = Object.keys(content)
   let element = Object.keys(content[type[0]])
   let result = []
+  console.log('type = ')
+  console.log(type)
+  console.log('element = ')
+  console.log(element)
   type.forEach(t => {
+    console.log('t = ')
+      console.log(t)
     let element = Object.keys(content[t])
     element.forEach(e => {
-      if (content[t][e].title.includes(text)) {
+      if (content[t][e].title.toLowerCase().includes(text.toLowerCase())) {
         result.push(content[t][e])
-      } else if (content[t][e].title.includes(text)) {
+      } else if (content[t][e].content.toLowerCase().includes(text.toLowerCase())) {
         result.push(content[t][e])
       }
     })
   })
+  console.log(result)
   return result
 }
